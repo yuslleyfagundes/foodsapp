85a819179886aa232dc0e3f979cc74645ae4b94b
yuslley <78389126+ysfagundes@users.noreply.github.com>
2021-02-27 14:31:53 -0300

    componets

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -10,6 +10,13 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Text(category.title),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          category.color.withOpacity(0.5),
+          category.color,
+        ],
+      )),
     );
   }
 }
